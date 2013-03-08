ea8cf9ff83709ef1ef6b3431c054854e09431a5e
Slater-Victoroff <slater.victoroff@students.olin.edu>
2013-03-08 16:52:35 -0500

    Bug fixes

--- cli.py
+++ cli.py
@@ -1,20 +1,24 @@
 import sys
 
-def parseCommandLineArguments(tagDictionary = {"-r":"recursive"}):
+def parseCommandLineArguments(tagDictionary = {"-r":"recursive"}, allArguments = sys.argv):
 	'''For now just allow the recursive tag, and assume every passed
 	directory should have the tag. Recursive should be -r.
 	Tags should be passed in the form of a dictionary mapping tag
 	with meaning. Output of this method should be a dictionary mapping all
 	filepaths to the results from the tagDictionary. All tags should start
 	with a '-' character, and nothing else should'''
-	allArguments = sys.argv
 	directoryDictionary = {}
-	for i in range(0,len(allArguments)):
+	for i in range(1,len(allArguments)):
 		if allArguments[i][0] != '-':
 			mapping = ""
 			for key in tagDictionary:
-				if allArguments[i+1] == key:
-					mapping = tagDictionary[key]
+				try:
+					if allArguments[i+1] == key:
+						mapping = tagDictionary[key]
+				except IndexError:
+					print "Near The End"
+					continue
 			directoryDictionary[allArguments[i]] = mapping
 	return directoryDictionary
 	
+print parseCommandLineArguments()
