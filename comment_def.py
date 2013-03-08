a4ba5e079e4be5bdafcfc3691febead8f753bda3
Justin Hewlett <hewlett.jus@gmail.com>
2013-03-07 20:17:57 -0700

    added comments

--- comment_def.py
+++ comment_def.py
@@ -1,3 +1,7 @@
+#this is my initial idea for how to specify the comment syntax for each language
+#this will allow us to look up in the dictionary by file extension
+#feedback is welcome
+
 class CommentDef:
     def __init__(self, block_regex, line_regex):
         self.block_regex = block_regex
