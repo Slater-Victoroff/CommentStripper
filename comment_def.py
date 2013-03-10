cf8e54c403c38b3e64264efce1f387b5f85893cb
Slater-Victoroff <slater.victoroff@students.olin.edu>
2013-03-10 10:56:51 -0400

    Added parsing of block comments

--- comment_def.py
+++ comment_def.py
@@ -4,18 +4,22 @@
 import re
 
 class CommentDef:
-    def __init__(self, block_regex, line_regex):
+    def __init__(self, blockRegex, lineRegex, blockContinuation=""):
         self.blockRegex = blockRegex
         self.lineRegex = lineRegex
+        self.blockContinuation = blockContinuation
         
 class CommentDictionary:	
 	def __init__(self):
-		cstyle = CommentDef('/\*(.|\r|\n)*?\*/', '([/]{2})(.*)')
-		python = CommentDef('([\']{3})([.|\r|\n]*?)([\']{3})', '(#)+(.*)')
-		perl = CommentDef('(=begin)([.|\r|\n]*?)(=cut)', '(#)+(.*)')
-		ruby = CommentDef('(=begin)([.|\r|\n]*?)(=end)', '(#)+(.*)')
-		lisp = CommentDef('(#|)([.|\r|\n]*?)(|#)', '(;)+(.*)')
+		cstyle = CommentDef('(/\*)(.|\r|\n)*?(?=(\*/))', '([/]{2})(.*)', "*")
+		python = CommentDef('([\']{3})([.|\r|\n]*?)(?=([\']{3}))', '(#)+(.*)')
+		perl = CommentDef('(=begin)([.|\r|\n]*?)(?=(=cut))', '(#)+(.*)')
+		ruby = CommentDef('(=begin)([.|\r|\n]*?)(?=(=end))', '(#)+(.*)')
+		lisp = CommentDef('(#|)([.|\r|\n]*?)(?=(|#))', '(;)+(.*)')
 		text = CommentDef('', '')
+		#All block comment should be indicated such that the closing
+		#element of the comment is in a lookahead assertion to allow
+		#for nested comments
 
 		self.reference = {'c': cstyle, 'cpp': cstyle, 'cs': cstyle,
 						'java': cstyle, 'js': cstyle, 'py': python, 
@@ -35,3 +39,20 @@ class CommentDictionary:
 						if re.match(regexPattern, line):
 							allComments.append(re.match(regexPattern, line).group(2))
 		return allComments
+		
+	def parseBlock(self, fileMappings):
+		'''Similar to parseInline, except this round looks for block comments'''
+		allComments = []
+		for key in fileMappings:
+			if fileMappings[key] in self.reference:
+				regexPattern = self.reference[key].blockRegex
+				with open(key, 'r').read() as corpus:
+					results = re.finditer(regexPattern, corpus)
+					for result in results:
+						cleanedResult = []
+						result = result.group(2).split('\n')
+						for line in result:
+							cleanedResult.append(line.strip()[len(self.reference[key].blockContinuation):].lower())
+						allComments.append(" ".join(cleanedResult))
+		return allComments
+					
